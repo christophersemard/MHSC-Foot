c01d91df4b1f7a330d38bf41f51a619484911e2f
ChristopherSemard <christophersemard76@gmail.com>
2022-07-25 16:42:49 +0200

    traduction cruds

--- src/Form/GalleryPostType.php
+++ src/Form/GalleryPostType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\File;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
@@ -15,8 +16,12 @@ class GalleryPostType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content', CKEditorType::class)
+            ->add('title', TextType::class, [
+                'label'     => 'Titre',
+            ])
+            ->add('content', CKEditorType::class, [
+                'label'     => 'Contenu',
+            ])
             ->add('imagePosts', FileType::class, [
                 'mapped' => false,
                 'multiple' => true,
@@ -42,6 +47,7 @@ class GalleryPostType extends AbstractType
 
             ])
             ->add('thumbnail', FileType::class, [
+                'label'     => 'Image de présentation',
                 'mapped' => false,
                 'required' => is_null($builder->getData()->getId()),
                 'constraints' => [
--- src/Form/VideoPostType.php
+++ src/Form/VideoPostType.php
@@ -6,16 +6,37 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 
 class VideoPostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+
             ->add('title')
-            ->add('content')
+            ->add('content', CKEditorType::class)
+
             ->add('video')
-            ->add('thumbnail')
+            ->add('thumbnail', FileType::class, [
+                'mapped' => false,
+                'required' => is_null($builder->getData()->getId()),
+                'constraints' => [
+                    new File([
+                        'maxSize' => '2048k',
+                        'mimeTypes' => [
+                            "image/png",
+                            "image/jpg",
+                            "image/jpeg",
+                            "image/gif",
+                            "image/webp"
+                        ],
+                        'mimeTypesMessage' => 'Merci de selectionner des images valides. (JPG, PNG, JPEG, GIF, WEBP) de moins de 2Mo.',
+                    ])
+                ],
+            ])
             ->add('category');
     }
 
