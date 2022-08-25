c28fd647dc156ca14051b4637933cf4494e50dc5
ChristopherSemard <christophersemard76@gmail.com>
2022-08-25 16:57:27 +0200

    request api football

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PostRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, HttpClientInterface $client, Request $request): Response
     {
+        // $response = $client->request(
+        //     'GET',
+        //     'https://api-football-v1.p.rapidapi.com/v3/teams',
+        //     [
+        //         'query' => ['id' => '82'],
+        //         'headers' => [
+        //             'x-rapidapi-host' => 'api-football-v1.p.rapidapi.com',
+        //             'x-rapidapi-key' => '1f02f9ca6amshccfc632c7c05802p1a48cbjsnb7f356338428'
+        //         ]
+        //     ]
+        // );
 
-
+        // $data = json_decode($response->getContent(), true);
+        // dd($data);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
