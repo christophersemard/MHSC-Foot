17ab56624003e9f295ac11e0b40bbe0c38109f0f
ChristopherSemard <christophersemard76@gmail.com>
2022-09-15 16:50:37 +0200

    fix

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\SinglePageRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\SinglePageCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
